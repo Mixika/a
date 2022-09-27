69251c27b8511113e78767831a6bf1cbff7cfd80
Mixika <113959868+Mixika@users.noreply.github.com>
2022-09-27 14:16:11 +0800

    second

---
+++ test3.c
@@ -0,0 +1,23 @@
+﻿////生命周期的概念
+////coust int num修饰是常量
+////const修饰的常量
+#include<stdio.h>;
+
+ int main()
+{
+	int a =0;
+	a  = 1;
+
+
+     printf("a = %d\n", a);
+
+
+ }
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	a = 2;
+//	printf("a=%d\n", a);
+//
+//}
